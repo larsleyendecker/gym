ae35b90258f24aa839575f5324d180d1e1fe2265
Marius <marius.hebecker@me.com>
2020-01-19 13:37:09 +0100

    adapted xml path

--- gym/envs/robotics/ur5/assemble.py
+++ gym/envs/robotics/ur5/assemble.py
@@ -2,9 +2,12 @@ import os
 from gym import utils
 from gym.envs.robotics import ur5_env
 import numpy as np
+import os
+
 
 # Ensure we get the path separator correct on windows
-MODEL_XML_PATH = '/home/marius/DRL_SetBot-RearVentilation/UR5+gripper/UR5_position_control.xml'
+HOME_PATH = os.getenv("HOME")
+MODEL_XML_PATH = os.path.join(*[HOME_PATH, "DRL_SetBot-RearVentilation", "UR5+gripper", "UR5_position_control.xml"])
 initial_qpos = np.array([1.5708, 0, -1.5708, 1.5708, 1.5708, 1.5708])
 
 class Ur5HegEnv(ur5_env.Ur5Env, utils.EzPickle):
