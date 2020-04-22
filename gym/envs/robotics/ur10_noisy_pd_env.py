9a2a597c22dcac62476453ed2d377b294340f2bc
Marius <marius.hebecker@me.com>
2020-04-22 22:01:43 +0200

    added noise to ft and position

--- gym/envs/robotics/ur10_noisy_pd_env.py
+++ gym/envs/robotics/ur10_noisy_pd_env.py
@@ -259,12 +259,12 @@ class Ur10Env(robot_custom_env.RobotEnv):
             ft += self.ft_drift_val
         ft[2] -= 0.350*9.81  # nulling in initial position
         x_pos = self.sim.data.get_body_xpos("gripper_dummy_heg")
-        x_pos += numpy.random.uniform(-self.pos_std, self.pos_std)
+        x_pos += numpy.random.uniform(-self.pos_std[:3], self.pos_std[:3])
         x_pos += self.pos_drift_val
 
         #x_quat = self.sim.data.get_body_xquat("gripper_dummy_heg")
         x_mat = self.sim.data.get_body_xmat("gripper_dummy_heg")
-        rpy = normalize_rad(rotations.mat2euler(x_mat))
+        rpy = normalize_rad(rotations.mat2euler(x_mat) + numpy.random.uniform(-self.pos_std[3:], self.pos_std[3:]))
 
         obs = np.concatenate([
             rot_mat.dot(x_pos-self.goal[:3]), rot_mat.dot(normalize_rad(rpy-self.goal[3:])), ft.copy()
